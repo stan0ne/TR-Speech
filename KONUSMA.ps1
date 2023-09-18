474883af31ec015839165f4e327cdcc879b05a2a
stan0ne <savas_boluk86@hotmail.com>
2023-09-18 12:29:46 +0300

    Add files via upload

---
+++ KONUSMA.ps1
@@ -0,0 +1,6 @@
+﻿Add-Type -AssemblyName System.speech
+$speak = New-Object System.Speech.Synthesis.SpeechSynthesizer
+$speak.SelectVoice('Microsoft Tolga')
+
+$speak.Speak("14 Mayıs seçimlerine ilişkin propaganda dönemi kural ve yasakları belli oldu.")
+$speak.Dispose()
